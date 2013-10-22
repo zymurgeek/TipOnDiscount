b446ecac59f21cd38ae163db96b530b0dbbfd332
David A. Greenbaum <zymurgeek@gmail.com>
2013-10-22 15:10:46 -0400

    Display Toast message if DataModelPersisterImpl.saveState() fails

--- App/src/com/itllp/tipOnDiscount/model/persistence/impl/DataModelPersisterImpl.java
+++ App/src/com/itllp/tipOnDiscount/model/persistence/impl/DataModelPersisterImpl.java
@@ -1,8 +1,27 @@
+// Copyright 2013 David A. Greenbaum
+/*
+This file is part of Tip On Discount.
+
+Tip On Discount is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+Tip On Discount is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Tip On Discount.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.itllp.tipOnDiscount.model.persistence.impl;
 
 import java.math.BigDecimal;
 
 import android.content.Context;
+import android.widget.Toast;
+
 import com.itllp.tipOnDiscount.model.DataModel;
 import com.itllp.tipOnDiscount.model.persistence.DataModelPersister;
 import com.itllp.tipOnDiscount.persistence.Persister;
@@ -29,15 +48,10 @@ public class DataModelPersisterImpl implements DataModelPersister {
 			persister.save(DataModel.ROUND_UP_TO_NEAREST_AMOUNT, 
 					model.getRoundUpToAmount());
 			persister.save(DataModel.BUMPS_KEY, model.getBumps());
-		} catch (Exception e) {
-			// TODO toast if failed
-//			Toast.makeText(this,
-//           		 "Failed to save state", Toast.LENGTH_LONG).show();
-		}
-		try {
 			persister.endSave();
 		} catch (Exception e) {
-			// TODO toast if failed
+			Toast.makeText(context,
+           		 "Failed to save state", Toast.LENGTH_LONG).show();
 		}
 	}
 
